605d717ad9f6c6be8e456aa762e2ea1a65c6daa1
DELL <akkikadwe@gmail.com>
2022-06-03 11:52:05 +0530

    for loop is added

--- firstfile.py
+++ firstfile.py
@@ -1,11 +1,18 @@
 print('Hello')
-a=10
-b=3
+a = 10
+b = 3
 print(a+b)
 
 # learning the basics of
-c=10
-# this things muste be added to the project
-d=11
+c = 10
+# This things must be added to the project
+d = 11
 #
-e=15
+e = 15
+
+# to four lines needs to be added
+a = 1
+
+for i in range(11):
+    a = a+1
+    print(a)
